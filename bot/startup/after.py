cb9656b14166ed1b83a20a12fdf69a8aa89b6954
Hotmoon <aravind1555.av@gmail.com>
2024-07-23 13:24:55 +0530

    Update after.py

--- bot/startup/after.py
+++ bot/startup/after.py
@@ -14,7 +14,7 @@ async def start_aria2p():
             aria2p.Client(host="http://localhost", port=conf.ARIA2_PORT, secret="")
         )
         aria2.add(
-            "https://nyaa.si/download/1752639.torrent",
+            "https://www.alucare.fr/wp-content/uploads/2023/08/Naruto-scaled.jpg",
             {"dir": f"{os.getcwd()}/temp"},
         )
         await asyncio.sleep(2)
@@ -29,18 +29,11 @@ async def start_aria2p():
         # return None
 
 
-async def start_qbit():
-    os.system(
-        f"qbittorrent-nox -d --webui-port={conf.QBIT_PORT} --profile={os.getcwd()}"
-    )
-    # TO_DO: Properly check if qbit is fully operational.
-    _bot.sqs = True
-
 
 async def start_rpc():
     os.system(
         f"aria2c --enable-rpc=true --rpc-max-request-size=1024M --rpc-listen-port={conf.ARIA2_PORT} "
-        "--seed-time=0 --follow-torrent=mem --summary-interval=0 --daemon=true --allow-overwrite=true "
+        "--seed-time=0 --summary-interval=0 --daemon=true --allow-overwrite=true "
         "--user-agent=Wget/1.12"
     )
     if not _bot.started:
@@ -84,8 +77,7 @@ async def onstart():
         try:
             await tele.send_message(
                 dev,
-                f"**Aria2:** `{'Online' if _bot.sas else 'Offline/Not_ready'}`"
-                f"\n**Qbit:** `{'Online' if _bot.sqs else 'Offline/Not_ready'}`",
+                f"**Bot Started!**",
             )
         except Exception:
             await logger(Exception)
@@ -93,33 +85,16 @@ async def onstart():
         pass
 
 
-async def on_termination():
-    try:
-        dead_msg = f"**I'm {enquip2()} {enmoji2()}**"
-        if conf.LOG_CHANNEL:
-            await tele.send_message((conf.LOG_CHANNEL), dead_msg)
-        else:
-            for i in conf.OWNER.split():
-                try:
-                    await tele.send_message(int(i), dead_msg)
-                except Exception:
-                    pass
-    except Exception:
-        pass
-    # More cleanup code?
-    exit(0)
-
 
 async def on_startup():
     try:
         asyncio.create_task(autostat())
         asyncio.create_task(start_rpc())
-        asyncio.create_task(start_qbit())
         loop = asyncio.get_running_loop()
         for signame in {"SIGINT", "SIGTERM", "SIGABRT"}:
             loop.add_signal_handler(
                 getattr(signal, signame),
-                lambda: asyncio.create_task(on_termination()),
+                ),
             )
         if len(sys.argv) == 3:
             await onrestart()
