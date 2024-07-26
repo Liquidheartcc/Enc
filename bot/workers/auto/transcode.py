8d55512f7cc5dd19ba47a879e024d04b7938863a
Hotmoon <aravind1555.av@gmail.com>
2024-07-26 13:05:55 +0530

    Update transcode.py

--- bot/workers/auto/transcode.py
+++ bot/workers/auto/transcode.py
@@ -38,7 +38,7 @@ import time
 import pickle
 import mimetypes
 from googleapiclient.discovery import build
-from googleapiclient.http import MediaIoBaseUpload
+from googleapiclient.http import MediaFileUpload
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 
@@ -46,6 +46,7 @@ from google.auth.transport.requests import Request
 SCOPES = ['https://www.googleapis.com/auth/drive.file']
 CREDENTIALS_FILE_PATH = 'bot/credentials.json'
 TOKEN_PICKLE_FILE_PATH = 'bot/token.pickle'
+ 
 
 def upload_to_gdrive(file_path, folder_id):
     """Uploads a file to Google Drive and returns the web view link."""
@@ -62,12 +63,10 @@ def upload_to_gdrive(file_path, folder_id):
         with open(TOKEN_PICKLE_FILE_PATH, 'wb') as token:
             pickle.dump(creds, token)
     service = build('drive', 'v3', credentials=creds)
-    file_mimetype, _ = mimetypes.guess_type(file_path)
-    media = MediaIoBaseUpload(open(file_path, 'rb'), mimetype=file_mimetype, resumable=True)
+    media = MediaFileUpload(file_path, resumable=True)
     file_metadata = {
         'name': os.path.basename(file_path),
         'parents': [folder_id],
-        'mimeType': file_mimetype
     }
     file = service.files().create(body=file_metadata, media_body=media, fields='id,webViewLink').execute()
     return file.get("webViewLink")
